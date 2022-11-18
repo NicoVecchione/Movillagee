dbd8dd41a9e2dfaaadc642809bdeb2bd1cca6e59
NicoVecchione <nvecchione@udesa.edu.ar>
2022-11-18 12:36:44 -0300

    Footer terminado

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -2,9 +2,6 @@ let queryString = location.search
 
 let qsObject = new (location.search)
 
-
-
-//endpoint con el id de la qs
 let url_detailMovie = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=35664717fe783f635e22f58af930e36f&language=en-US"
 
 //fetch
