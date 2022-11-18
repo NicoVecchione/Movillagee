3f8fce6b61886edb8893ccdefb116fdfe4c75d1e
FelipeAntelme <fantelmedeanchorena@udesa.edu.ar>
2022-11-18 18:44:59 -0300

    fetch detail-movie

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,10 +1,5 @@
-let queryString = location.search
-
-let qsObject = new (location.search)
-
 let url_detailMovie = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=35664717fe783f635e22f58af930e36f&language=en-US"
 
-//fetch
 fetch(url_detailMovie)
     .then(function(res){
         return response.json()
@@ -12,7 +7,17 @@ fetch(url_detailMovie)
     })
     .then(function(data) {
         console.log(data);
-        let     
+        let url_imagen = "https://image.tmdb.org/t/p/original"
+        let detalles = document.querySelector(".contenedor-texto")
+        let detalles_pelis = ``
+
+        for (let i=0; i<2; i++){
+            detalles_pelis += `<div class="contenedor-texto">
+            
+            `
+        }
+        detalles.innerHTML = detalles_pelis
+        
     })
     .catch(function(error) {
         console.log("Error: " + error);
