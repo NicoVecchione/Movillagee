88cf109bd34022ec1ad1a433af4ede653787e365
NicoVecchione <nvecchione@udesa.edu.ar>
2022-11-19 22:50:44 -0300

    Buscador en todas las paginas

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,3 +1,27 @@
+// FORMULARIO
+
+let buscador = document.querySelector(".buscador")
+
+let formulario = document.querySelector(".busqueda")
+let error = document.querySelector(".error")
+formulario.addEventListener("submit", function(event){
+    event.preventDefault()
+    // Que no se mande si no hay nada en el campo
+    if(buscador.value == ""){
+        error.innerText ="Complete la busqueda!"
+            // Mensaje de error desaparece
+            buscador.addEventListener("focus", function(){
+                error.innerText = null
+    // Que tenga al menos 3 caracteres
+    }) } else if (buscador.value.length < 3 && buscador.value !== ""){
+        error.innerText= "Debe introducir minimo 3 caracteres";
+            // Mensaje de error desaparece
+            buscador.addEventListener("focus", function(){
+                error.innerText = null
+    }) } else {
+        this.submit()
+    }
+})
 let queryString = location.search
 let qsObject = new URLSearchParams(queryString)
 let id = qsObject.get('id')
