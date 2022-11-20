dbe6905484f9b2aac9c1ffb799245a72cbb9cb21
NicoVecchione <nvecchione@udesa.edu.ar>
2022-11-20 01:50:38 -0300

    Avance favoritos

--- js/favorites.js
+++ js/favorites.js
@@ -22,3 +22,41 @@ formulario.addEventListener("submit", function(event){
         this.submit()
     }
 })
+
+// FAVORITOS
+let recuperoStorage = localStorage.getItem("pelisfavoritas")
+let peliculas = JSON.parse(recuperoStorage)
+
+let listaFavs = document.querySelector(".FavsPelis")
+
+if(peliculas == null){
+    listaFavs.innerHTML = `<p> No tenes Favoritos </p>`
+} else {
+    for(let i=0; i<peliculas.length; i++){
+        buscarYMostrarFavoritos(peliculas[i])
+    }
+}
+
+function buscarYMostrarFavoritos(id){
+
+    let url = `https://api.themoviedb.org/3/account/${id}/favorite/movies?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=created_at.asc&page=1`
+    let url2 = `https://api.themoviedb.org/3/movie/${id}?api_key=e3f1ae8bae04c04c63af7b6996decd02&language=en-US`
+    fetch(url2)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data);
+        let clave = data.results
+        let urlimg = "https://image.tmdb.org/t/p/original"
+        listaFavs += `<article class= "Favstodas">
+                        <a href="detail-movie.html?id=${clave.id}"><img class="pelisFavs" src=${urlimg + clave.poster_path} alt=${clave.original_title}></a>
+                        <strong> ${clave.original_title} </strong>
+                        <p> ${clave.release_date} </p>
+                     </article>`
+    })
+    .catch(function(e){
+        console.log(e)
+    })
+
+}
