46c0f6b77dab2c40657be5dd3aba318f3098b9e9
NicoVecchione <nvecchione@udesa.edu.ar>
2022-11-20 21:29:48 -0300

    Favoritos

--- js/favorites.js
+++ js/favorites.js
@@ -26,33 +26,35 @@ formulario.addEventListener("submit", function(event){
 // FAVORITOS
 let recuperoStorage = localStorage.getItem("pelisfavoritas")
 let peliculas = JSON.parse(recuperoStorage)
+console.log(peliculas)
 
 let listaFavs = document.querySelector(".FavsPelis")
 
+// Me fijo si el array tiene elementos (pelis)
 if(peliculas == null){
+    //Si no hay favoritos
     listaFavs.innerHTML = `<p> No tenes Favoritos </p>`
 } else {
-    for(let i=0; i<peliculas.length; i++){
-        buscarYMostrarFavoritos(peliculas[i])
+    //Si hay favoritos
+    for(let i=0; i<peliculas.length; i++){ // El for va a ir recorriendo cada posicion del array y le aplica la funcion
+        buscarYMostrarFavoritos(peliculas[i]) // peliculas [i] es = a "id"
     }
 }
 
-function buscarYMostrarFavoritos(id){
+function buscarYMostrarFavoritos(id){ 
 
-    let url = `https://api.themoviedb.org/3/account/${id}/favorite/movies?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=created_at.asc&page=1`
-    let url2 = `https://api.themoviedb.org/3/movie/${id}?api_key=e3f1ae8bae04c04c63af7b6996decd02&language=en-US`
-    fetch(url2)
+    let url = `https://api.themoviedb.org/3/movie/${id}?api_key=e3f1ae8bae04c04c63af7b6996decd02&language=en-US`
+    fetch(url)
     .then(function(res){
         return res.json()
     })
     .then(function(data){
         console.log(data);
-        let clave = data.results
         let urlimg = "https://image.tmdb.org/t/p/original"
-        listaFavs += `<article class= "Favstodas">
-                        <a href="detail-movie.html?id=${id}"><img class="pelisFavs" src=${urlimg + poster_path} alt=${original_title}></a>
-                        <strong> ${original_title} </strong>
-                        <p> ${release_date} </p>
+        listaFavs.innerHTML += `<article class= "popu">
+                        <a href="detail-movie.html?id=${data.id}"><img class="pelis" src=${urlimg + data.poster_path} alt=${data.title}></a>
+                        <strong> ${data.title} </strong>
+                        <p class= "estreno"> ${data.release_date} </p>
                      </article>`
     })
     .catch(function(e){
@@ -60,3 +62,4 @@ function buscarYMostrarFavoritos(id){
     })
 
 }
+
