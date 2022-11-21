cabeaac1e66df872b356fb0890ac00f54a1b7403
MateLavan <lavandeiram@udesa.edu.ar>
2022-11-21 15:03:20 -0300

    funcionalidad del search

--- js/search-result.js
+++ js/search-result.js
@@ -1,5 +1,5 @@
 // FORMULARIO
-
+let contenedor=document.querySelector(".populares")
 let buscador = document.querySelector(".buscador")
 
 let formulario = document.querySelector(".busqueda")
@@ -19,6 +19,23 @@ formulario.addEventListener("submit", function(event){
             buscador.addEventListener("focus", function(){
                 error.innerText = null
     }) } else {
-        this.submit()
+        let moviename=buscador.value   
+      getMovieByName (moviename).then (function(data){
+     data.json().then(function(respuesta){
+        respuesta.results.forEach(pelicula => {
+            contenedor.innerHTML+=`
+            <article class="popu">
+            <a href="./detail-movie.html"><img class="pelis" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" alt="Spider"></a>
+            <h3>${pelicula.title}</h3>
+        </article>
+            `
+        });
+console.log(respuesta)
+     })
+      })
     }
 })
+function getMovieByName (moviename){
+   return fetch ("https://api.themoviedb.org/3/search/movie?api_key=faebca224880d1a1cf1fe2cb4230ae6d&language=en-US&page=1&include_adult=false&query="+ moviename)
+
+}
